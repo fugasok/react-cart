f02a8aca1d2fc7b68d6e5b38a64bc868cf34a335
Tibor <tibor736@hotmail.com>
2023-07-19 13:49:46 +0300

    refactored items in cart and added clear cart function with reducer

--- src/context.jsx
+++ src/context.jsx
@@ -1,5 +1,6 @@
 import { useContext, useReducer, useEffect, createContext } from "react";
 import reducer from "./reducer";
+import cartItems from "./data";
 import {
 	CLEAR_CART,
 	REMOVE_ITEM,
@@ -13,14 +14,21 @@ const AppContext = createContext();
 
 const initialState = {
 	loading: false,
-	cart: [],
+	cart: new Map(cartItems.map((item) => [item.id, item])),
 };
+3;
 
 export const AppProvider = ({ children }) => {
 	const [state, dispatch] = useReducer(reducer, initialState);
 
+	const clearCart = () => {
+		dispatch({ type: CLEAR_CART });
+	};
+
 	return (
-		<AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
+		<AppContext.Provider value={{ ...state, clearCart }}>
+			{children}
+		</AppContext.Provider>
 	);
 };
 
